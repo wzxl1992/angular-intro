49f6143cbb7b46d90c970eb8f742b7496d607f44
wzxl1992 <xulichou@gmail.com>
2014-11-03 12:11:50 -0800

    class notes

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@
 
 "use strict";
 
+//create bew angular module
+angular.module('MovieApp',[])
+    .controller('MoviesController', function($scope){
+        $scope.movies = movies;
+        $scope.searchString = '';
+        $scope.sortCol = 'rank';
+        $scope.sortReverse = false;
+
+        $scope.sortBy = function(colName) {
+            if($scope.sortCol == colName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            }
+
+            else {
+                $scope.sortReverse = false;
+            }
+
+            $scope.sortCol = colName;
+        };
+    });
